0e830f14651fcc69d87e60ef344302c6537c341b
kimberly owens <kimberlyowens@MacBook-Pro-2.attlocal.net>
2017-11-26 09:35:47 -0500

    updating middleware

--- app.js
+++ app.js
@@ -42,3 +42,27 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cookieParser());
+
+//Set Static Folder
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+//Express Validator
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.')
+            , root = namespace.shift()
+            , formParam = root;
+        while(namespace.length) {
+            formParam += '[' + namespace.shift() * ']';
+        }
+        return {
+            param : formParam,
+            msg : msg,
+            value : value
+        };
+    }
+}));
