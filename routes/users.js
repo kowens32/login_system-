a634b1731c6b42ba544f1eb49f6e72465978ec54
kimberly owens <kimberlyowens@MacBook-Pro-2.attlocal.net>
2017-11-27 10:21:29 -0500

    updating login

--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,26 @@ router.get('/login', function(req,res){
     res.render('login');
 });
 
+//register user
+router.post('/register', function(req,res){
+   var name = req.body.name;
+   var email = req.body.email;
+   var username = req.body.email;
+   var password = req.body.password;
+   var password2 = req.body.password2;
+
+
+   //Validation
+    req.checkBody('name', 'Name is required').notEmpty();
+
+    var errors = req.validationErrors();
+
+    if(errors){
+        console.log('YES');
+    } else{
+        console.log('NO')
+    }
+
+});
+
 module.exports = router;
